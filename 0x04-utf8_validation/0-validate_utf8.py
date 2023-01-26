b8838339e5ecfc01363b64fd0eaca004d83c59c0
stephen <stevanion2013@gamil.com>
2023-01-26 04:50:19 -0800

    valid UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,27 @@
 #!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
+""" UTF-8 Validation."""
 
 
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
+        Determines if a given data set represents a valid UTF-8 encoding.
     """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
-
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
+    i = 0
+    while i < len(data):
+        byte_count = 0
+        if (data[i] & 128) == 0:
+            byte_count = 1
+        elif (data[i] & 224) == 192:
+            byte_count = 2
+        elif (data[i] & 240) == 224:
+            byte_count = 3
+        elif (data[i] & 248) == 240:
+            byte_count = 4
+        else:
             return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+        for j in range(1, byte_count):
+            if i+j >= len(data) or (data[i+j] & 192) != 128:
                 return False
-            if byte[0:2] != [1, 0]:
-                return False
+        i += byte_count
     return True
